72638603c6ba60a3cf206f58745a381802fadf3d
Philipp Seibt <seibt@pdir.de>
2018-04-11 14:28:41 +0200

    initial commit

---
+++ _local_bundle/pdir/theme-odd-bundle/src/PdirThemeOddBundle.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * pdir theme odd bundle for Contao Open Source CMS
+ *
+ * Copyright (C) 2018 pdir / digital agentur <develop@pdir.de>
+ *
+ * @package    theme odd bundle
+ * @link       https://pdir.de
+ * @license    pdir license - All-rights-reserved - commercial extension
+ * @author     pdir GmbH <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pdir\ThemeOddBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the bundle.
+ *
+ * @author Philipp Seibt <seibt@pdir.de>
+ */
+class PdirThemeOddBundle extends Bundle
+{
+}
