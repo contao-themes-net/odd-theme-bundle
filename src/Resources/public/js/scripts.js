9b08544c97422be318bc6c4f4f5453fbbdf10363
Philipp Seibt <seibt@pdir.de>
2018-04-16 18:00:41 +0200

    update templates, css and js

--- src/Resources/public/js/scripts.js
+++ src/Resources/public/js/scripts.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
 
-    $("#main > .inside > div:not(.fullwidth)").each( function() {
-        $(this).wrapInner("<div class='container'></div>");
+    /* ===================== *
+     *   Toplink  			 *
+     * ====================== */
+    $(window).scroll(function () {
+        scrollPos = $(document).scrollTop();
+
+        $("footer .toplink").addClass("active");
+        if(scrollPos <= 500) {
+            $("footer .toplink").removeClass("active");
+        }
+    });
+
+    $(document).on('click', 'footer .toplink', function(event){
+        event.preventDefault();
+
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+    });
+
+    /* =================== *
+     * Smooth Scroll	   *
+     * =================== */
+    $('a[href*=\\#]').on('click', function(event){
+        var href = $(this).attr('href');
+        href = href.substr(0,href.indexOf('#'));
+        href = href.replace('./','');
+
+        var path = window.location.pathname;
+        path = path.replace('/','');
+
+        if ( $(this).attr('target') != '_blank' && path == href) {
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+        }
     });
 
 });
