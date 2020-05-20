2857188fb996ef40f49400b4b47dca8b7256ae77
Philipp Seibt <seibt@pdir.de>
2020-05-20 16:39:43 +0200

    remove background if footer newsletter section is hidden

--- src/Module/OddThemeSetup.php
+++ src/Module/OddThemeSetup.php
@@ -4,7 +4,7 @@ namespace Pdir\ThemeOddBundle\Module;
 
 class OddThemeSetup extends \BackendModule
 {
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.1';
 
     protected $strTemplate = 'be_oddtheme_setup';
 
