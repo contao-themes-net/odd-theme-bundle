66468225678efba4a6602745dc189f7990ccabe5
Philipp Seibt <seibt@pdir.de>
2021-01-04 16:17:01 +0100

    update sql files (set authors)

--- src/Module/OddThemeSetup.php
+++ src/Module/OddThemeSetup.php
@@ -4,7 +4,7 @@ namespace Pdir\ThemeOddBundle\Module;
 
 class OddThemeSetup extends \BackendModule
 {
-    const VERSION = '1.4.7';
+    const VERSION = '1.4.8';
 
     protected $strTemplate = 'be_oddtheme_setup';
 
