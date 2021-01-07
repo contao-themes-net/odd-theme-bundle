1afa2c35aff138fbd240c5748a1fdf583041d4dc
Philipp Seibt <seibt@pdir.de>
2021-01-07 18:01:53 +0100

    fix button css

--- src/Module/OddThemeSetup.php
+++ src/Module/OddThemeSetup.php
@@ -4,7 +4,7 @@ namespace Pdir\ThemeOddBundle\Module;
 
 class OddThemeSetup extends \BackendModule
 {
-    const VERSION = '1.4.8';
+    const VERSION = '1.4.9';
 
     protected $strTemplate = 'be_oddtheme_setup';
 
