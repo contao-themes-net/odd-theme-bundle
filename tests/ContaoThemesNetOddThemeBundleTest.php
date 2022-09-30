939d8aa5f8ec6ad5428dd09d27d74c8587ad8db3
Mathias Arzberger <develop@pdir.de>
2022-09-30 21:17:13 +0200

    add extension test

---
+++ tests/ContaoThemesNetOddThemeBundleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * pdir theme odd bundle for Contao Open Source CMS
+ *
+ * Copyright (C) 2022 pdir / digital agentur <develop@pdir.de>
+ *
+ * @package    theme odd bundle
+ * @link       https://github.com/contao-themes-net/odd-theme-bundle
+ * @license    pdir contao theme licence
+ * @author     pdir GmbH <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ContaoThemesNet\OddThemeBundle\Tests;
+
+use ContaoThemesNet\OddThemeBundle\DependencyInjection\ContaoThemesNetOddThemeExtension;
+use PHPUnit\Framework\TestCase;
+
+class ContaoThemesNetOddThemeBundleTest extends TestCase
+{
+    public function testCanBeInstantiated(): void
+    {
+        $bundle = new ContaoThemesNetOddThemeExtension();
+
+        $this->assertInstanceOf('ContaoThemesNet\OddThemeBundle\ContaoThemesNetOddThemeExtension', $bundle);
+    }
+}
