edd89c48d55f305d8cb1c9e61c199e54e3912637
Philipp Seibt <seibt@pdir.de>
2023-01-03 11:41:00 +0100

    change favicon

--- src/Module/OddThemeSetup.php
+++ src/Module/OddThemeSetup.php
@@ -22,7 +22,7 @@ use Contao\BackendModule;
 
 class OddThemeSetup extends BackendModule
 {
-    public const VERSION = '3.0.1';
+    public const VERSION = '3.0.2';
 
     protected $strTemplate = 'be_oddtheme_setup';
 
