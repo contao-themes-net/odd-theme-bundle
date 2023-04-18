bb750dd2f8573c1cd13d972181599d74720c7fa6
Jan Boech <info@jan-boesch.net>
2023-04-18 15:57:39 +0200

    Create modules.php

---
+++ contao/languages/fr/modules.php
@@ -0,0 +1,17 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['themeOdd'] = 'Thème ODD';
+$GLOBALS['TL_LANG']['CTE']['sliderElement'][0] = 'Elément du slider';
+
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD01/01'] = 'Entête / 01';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD01/02'] = 'Entête / 02';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD02/01'] = 'Pied de page / 01';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD02/02'] = 'Pied de page / 02';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD02/03'] = 'Pied de page / 03';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD02/03'] = 'Pied de page / 04';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['ODD02/03'] = 'Pied de page / 05';
+
+$GLOBALS['TL_LANG']['MOD']['oddTheme'][0] = "Thèmes pdir";
+$GLOBALS['TL_LANG']['MOD']['oddThemeSetup'][0] = "Configuration du thème ODD";
+
+$GLOBALS['TL_LANG']['odd']['event_more_link'] = "Afficher les détails de l'événement";
