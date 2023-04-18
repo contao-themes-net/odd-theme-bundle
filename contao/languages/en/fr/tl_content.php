042a010ed0d278bf2ce7d420ca226ff6232320ea
Jan Boech <info@jan-boesch.net>
2023-04-18 15:55:50 +0200

    Create tl_content.php

---
+++ contao/languages/en/fr/tl_content.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['odd_page'] = ['Page à lier','Sélectionnez la page à lier.'];
+$GLOBALS['TL_LANG']['tl_content']['odd_linkText'] = ['Texte du lien',''];
+$GLOBALS['TL_LANG']['tl_content']['target'] = ['Ouvrir dans une nouvelle fenêtre','Ouvrir le lien dans une nouvelle fenêtre de navigateur.'];
+$GLOBALS['TL_LANG']['tl_content']['odd_subHeadline'] = ['Sous-titre','Ici, vous pouvez ajouter un sous-titre à l\'élément de contenu.'];
