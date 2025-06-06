4042f369aebcddf7dc94da8ca0f2ba0f94e64953
Philipp Seibt <seibt@pdir.de>
2025-06-06 14:31:36 +0200

    update changelog and version

--- src/Module/OddThemeSetup.php
+++ src/Module/OddThemeSetup.php
@@ -22,7 +22,7 @@ use Contao\BackendModule;
 
 class OddThemeSetup extends BackendModule
 {
-    public const VERSION = '3.6.0';
+    public const VERSION = '3.7.0';
 
     protected $strTemplate = 'be_oddtheme_setup';
 
