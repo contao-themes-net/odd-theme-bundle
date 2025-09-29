4b162ba89a732b7c573b96a209fe20277dfc2a07
Philipp Seibt <seibt@pdir.de>
2025-09-29 18:10:30 +0200

    Added SQL files to enable demo import in Contao 5.6

--- src/Module/OddThemeSetup.php
+++ src/Module/OddThemeSetup.php
@@ -22,7 +22,7 @@ use Contao\BackendModule;
 
 class OddThemeSetup extends BackendModule
 {
-    public const VERSION = '3.7.2';
+    public const VERSION = '3.7.3';
 
     protected $strTemplate = 'be_oddtheme_setup';
 
